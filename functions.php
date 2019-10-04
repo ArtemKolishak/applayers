8181382b0f4c22ffc3521311459675e9b75d2ad5
Artem Kolishchak <artiom7510@gmail.com>
2019-10-04 19:35:05 +0300

    ADD: features wordpress(functions.php file)

--- functions.php
+++ functions.php
@@ -7,6 +7,82 @@
  * @package applayers
  */
 
+if ( ! function_exists( 'applayers_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function applayers_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on applayers, use a find and replace
+		 * to change 'applayers' to the name of your theme in all the template files.
+		 */
+        load_theme_textdomain( 'applayers', get_template_directory() . '/languages' );
+
+        /*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+        add_theme_support( 'title-tag' );
+
+        /* 
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+        add_theme_support( 'post-thumbnails' );
+        set_post_thumbnail_size( 730, 446 );
+
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support( 'html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ) );
+
+        /*
+         * Enable support for Post Formats
+         * 
+         * @link https://developer.wordpress.org/themes/functionality/post-formats/
+         */
+        add_theme_support( 'post-formats', array(
+            'aside',
+            'gallery',
+            'link',
+            'image',
+            'quote',
+            'video',
+            'audio',
+            'chat'  
+        ) );
+        
+        /**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 31,
+			'width'       => 134,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+    }
+endif;
+add_action( 'after_setup_theme', 'applayers_setup' );
+
  /**
  * Enqueue scripts and styles.
  */
