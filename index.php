b252899aef1c2fa5c0a87e319530adf5de4cb425
Artem Kolishchak <artiom7510@gmail.com>
2019-10-09 21:37:37 +0300

    ADD: Pagination

--- index.php
+++ index.php
@@ -21,50 +21,43 @@
 			<div class="col-md-12">
         		<div class="col-md-8">
 
-				<?php if ( have_posts() ) :?> 
-					<?php while ( have_posts() ) : the_post(); ?>
-						<article class="blog_post">
-							<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-						
-							<div class="blog_category">
-								<ul> 
-									<li><?php the_category(', '); ?></li>
-								</ul>
-							</div>	
+					<?php if ( have_posts() ) :?> 
+						<?php while ( have_posts() ) : the_post(); ?>
+							<article class="blog_post">
+								<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+							
+								<div class="blog_category">
+									<ul> 
+										<li><?php the_category(', '); ?></li>
+									</ul>
+								</div>	
 
-							<div class="blog_text">
-								<ul>
-									<li> | </li>
-									<li>Post By : <?php the_author_posts_link(); ?></li>
-									<li> | </li>
-									<li>On : <?php the_time( 'j F Y' ); ?></li>
-								</ul>
-							</div>
+								<div class="blog_text">
+									<ul>
+										<li> | </li>
+										<li>Post By : <?php the_author_posts_link(); ?></li>
+										<li> | </li>
+										<li>On : <?php the_time( 'j F Y' ); ?></li>
+									</ul>
+								</div>
+							
+								<div class="blog_post_img">
+									<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+								</div>
+							
+								<?php the_excerpt(); ?>
 						
-							<div class="blog_post_img">
-								<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-							</div>
-						
-							<?php the_excerpt(); ?>
+								<a href="<?php the_permalink(); ?>">Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+							</article>
 					
-							<a href="<?php the_permalink(); ?>">Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-						</article>
-				
-                	<?php endwhile; ?>
-        		<?php endif; ?>						
-				
-					<div class="next_page">
-          				<ul class="page-numbers">
-							<li><span class="page-numbers current">1</span></li>
-							<li><a href="#" class="page-numbers">2</a></li>
-							<li><a href="#" class="page-numbers">3</a></li>
-							<li><a href="#" class="page-numbers">4</a></li>
-							<li><a href="#" class="next page-numbers">Next</a></li>
-						</ul>
-       				</div>
+						<?php endwhile; ?>
+					<?php endif; ?>	
+
+					<?php applayers_pagination(); ?>
+
 				</div>	
-			
-            <?php get_sidebar(); ?>
+				
+				<?php get_sidebar(); ?>
 				
 			</div>
 		</div>
