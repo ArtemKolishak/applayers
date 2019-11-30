21ba3e824953dbc7be4d1e9b544ede0f5eaa449e
Artem Kolishchak <artiom7510@gmail.com>
2019-11-30 21:12:59 +0200

    ADD:Sidebar and Widget Categories; Register widget area.

--- functions.php
+++ functions.php
@@ -94,33 +94,33 @@ function applayers_scripts() {
     
     wp_enqueue_style( 'applayers-style', get_stylesheet_uri() );
 
-	/*Bootstrap CSS File*/
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri(). '/assets/lib/bootstrap/css/bootstrap.min.css', array( 'applayers-style' ) );
+	//Bootstrap CSS File
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri(). '/assets/libs/bootstrap/css/bootstrap.min.css', array( 'applayers-style' ) );
     
-	/*Font-Awesome CSS File*/
-	wp_enqueue_style( 'font-awesome', get_template_directory_uri(). '/assets/lib/font-awesome/css/font-awesome.min.css', array( 'applayers-style' ) );
+	//Font-Awesome CSS File
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri(). '/assets/libs/font-awesome/css/font-awesome.min.css', array( 'applayers-style' ) );
 
-	/*Animate.CSS CSS File*/
-	wp_enqueue_style( 'animate', get_template_directory_uri(). '/assets/lib/animate.css/animate.min.css', array( 'applayers-style' ) );
+	//Animate.CSS CSS File
+	wp_enqueue_style( 'animate', get_template_directory_uri(). '/assets/libs/animate.css/animate.min.css', array( 'applayers-style' ) );
 
-	/*Main Stylesheet File*/
+	//Main Stylesheet File
 	wp_enqueue_style( 'applayers-main-style', get_template_directory_uri(). '/assets/css/style.css', array( 'applayers-style' ) );
 
 
     /*JavaScript Libraries*/
 
-    /*jQuery*/
+    //jQuery
     wp_deregister_script( 'jquery' );
     wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, '', true );
     wp_enqueue_script( 'jquery' );
 
-    /*Bootstrap JS*/
-    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/lib/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '', true );
+    //Bootstrap JS
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/libs/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '', true );
 
-    /*CSS3-Animate-It*/
-    wp_enqueue_script( 'css3-animate-it', get_template_directory_uri() . '/assets/lib/css3-animate-it/css3-animate-it.js', array( 'jquery' ), '', true);
+    //CSS3-Animate-It
+    wp_enqueue_script( 'css3-animate-it', get_template_directory_uri() . '/assets/libs/css3-animate-it/css3-animate-it.js', array( 'jquery' ), '', true);
 
-    /*JS Plugins(Jquery)*/
+    //JS Plugins(Jquery)
     wp_enqueue_script( 'jquery-easing', get_template_directory_uri() . '/assets/js/jquery.easing.min.js', array( 'jquery' ), '', true);
 
     wp_enqueue_script( 'agency', get_template_directory_uri() . '/assets/js/agency.js', array( 'jquery' ), '', true);
@@ -136,309 +136,48 @@ add_filter( 'excerpt_more', function($more) {
 	return '';
 } );
 
-/**
- * Theme Breadcrumbs.
- */
-function applayers_the_breadcrumbs(){
-    global $post;
-    if(!is_home()){ 
-        echo 
-            '<li><a href="'.site_url() . '"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li>
-            <li> / </li>' ;
-            if(is_single()){ // posts
-            the_category( ', ' );
-            echo "<li> / </li>";
-            echo '<li> ';
-            the_title();
-            echo '</li> ';
-            }
-            elseif (is_page()) { // pages
-                if ($post->post_parent ) {
-                    $parent_id  = $post->post_parent;
-                    $breadcrumbs = array();
-                    while ($parent_id) {
-                        $page = get_page($parent_id);
-                        $breadcrumbs[] = '<li><a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></li>';
-                        $parent_id  = $page->post_parent;
-                    }
-                    $breadcrumbs = array_reverse($breadcrumbs);
-                    foreach ($breadcrumbs as $crumb) echo $crumb . ' <li> / </li>; ';
-                }
-                echo the_title();
-            }
-            elseif (is_category()) { // category
-                global $wp_query;
-                $obj_cat = $wp_query->get_queried_object();
-                $current_cat = $obj_cat->term_id;
-                $current_cat = get_category($current_cat);
-                $parent_cat = get_category($current_cat->parent);
-                if ($current_cat->parent != 0) 
-                    echo(get_category_parents($parent_cat, TRUE, ' <li> / </li> '));
-                        single_cat_title();
-                }
-                elseif (is_search()) { // search pages
-                    echo 'Search results "' . get_search_query() . '"';
-                }
-                elseif (is_tag()) { // tags
-                    echo single_tag_title('', false);
-                }
-                elseif (is_day()) { // archive (days)
-                    echo '
-                        <li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> 
-                        <li> / </li> ' ;
-                    echo 
-                        '<li><a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a></li> 
-                        <li> / </li> ';
-                    echo get_the_time( 'd' );
-                }
-                elseif (is_month()) { // archive (months)
-                    echo 
-                        '<li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> 
-                        <li> / </li> ';
-                    echo get_the_time( 'F' );
-                }
-                elseif (is_year()) { // archive (years)
-                    echo get_the_time( 'Y' );
-                }
-                elseif (is_author()) { // authors
-                    global $author;
-                    $userdata = get_userdata($author);
-                    echo '<li>Posted ' . $userdata->display_name . '</li>';
-                } 
-                elseif (is_404()) { // if page not found
-                    echo '<li>Error 404</li>';
-                }
-                if (get_query_var('paged')) // number of page
-                    echo ' (' . get_query_var('paged' ).'- page)';
-                } 
-    else { // home
-        $pageNum=(get_query_var('paged')) ? get_query_var('paged') : 1;
-        if($pageNum>1)
-            echo 
-                '<li><a href="'.site_url().'"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li>;
-                <li> / </li>  <li>'.$pageNum.'- page</li>';
-            else 
-                echo '<li><i class="fa fa-home" aria-hidden="true"></i>Home</li>';
-    }
-}
 
 /**
- * Delete site name at end of title
- */
-add_filter('document_title_parts', function( $parts ){
-	if( isset($parts['site']) ) unset($parts['site']);
-	return $parts;
-});
-
-/**
- * Theme  Pagination.
- */
-function applayers_pagination( $args = array() ) {
-    
-    $defaults = array(
-        'range'           => 4,
-        'custom_query'    => FALSE,
-        'previous_string' => __( 'Previous', 'text-domain' ),
-        'next_string'     => __( 'Next', 'text-domain' ),
-        'before_output'   => '<div class="next_page"><ul class="page-numbers">',
-        'after_output'    => '</ul></div>'
-    );
-    
-    $args = wp_parse_args( 
-        $args, 
-        apply_filters( 'applayers_pagination_defaults', $defaults )
-    );
-    
-    $args['range'] = (int) $args['range'] - 1;
-    if ( !$args['custom_query'] )
-        $args['custom_query'] = @$GLOBALS['wp_query'];
-    $count = (int) $args['custom_query']->max_num_pages;
-    $page  = intval( get_query_var( 'paged' ) );
-    $ceil  = ceil( $args['range'] / 2 );
-    
-    if ( $count <= 1 )
-        return FALSE;
-    
-    if ( !$page )
-        $page = 1;
-    
-    if ( $count > $args['range'] ) {
-        if ( $page <= $args['range'] ) {
-            $min = 1;
-            $max = $args['range'] + 1;
-        } elseif ( $page >= ($count - $ceil) ) {
-            $min = $count - $args['range'];
-            $max = $count;
-        } elseif ( $page >= $args['range'] && $page < ($count - $ceil) ) {
-            $min = $page - $ceil;
-            $max = $page + $ceil;
-        }
-    } 
-    else {
-        $min = 1;
-        $max = $count;
-    }
-    
-    $echo = '';
-    $previous = intval($page) - 1;
-    $previous = esc_attr( get_pagenum_link($previous) );
-    
-    if ( $previous && (1 != $page) )
-        $echo .= '<li><a href="' . $previous . '" class="page-numbers" title="' . __( 'previous', 'text-domain') . '">' . $args['previous_string'] . '</a></li>';
-    
-    if ( !empty($min) && !empty($max) ) {
-        for( $i = $min; $i <= $max; $i++ ) {
-            if ($page == $i) {
-                $echo .= '<li class="active"><span class="page-numbers current">' . str_pad( (int)$i, 1, '0', STR_PAD_LEFT ) . '</span></li>';
-            } 
-            else {
-                $echo .= sprintf( '<li><a href="%s" class="page-numbers">%2d</a></li>', esc_attr( get_pagenum_link($i) ), $i );
-            }
-        }
-    }
-    
-    $next = intval($page) + 1;
-    $next = esc_attr( get_pagenum_link($next) );
-    if ($next && ($count != $page) )
-        $echo .= '<li><a href="' . $next . '" class="page-numbers" title="' . __( 'next', 'text-domain') . '">' . $args['next_string'] . '</a></li>';
-    
-    if ( isset($echo) )
-        echo $args['before_output'] . $echo . $args['after_output'];
-}
-
-/**
- * Theme Customizer.
+ * Register widget area.
+ *
+ * Add a sidebar.
  * 
- * @link https://codex.wordpress.org/Theme_Customization_API
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
+function applayers_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Main Sidebar', 'applayers' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'applayers' ),
+        'before_widget' => '<div id="%1$s" class="sidebar_wrap %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<div class="side_bar_heading"><h6>',
+        'after_title'   => '</h6></div>',
+    ) );
+}
+add_action( 'widgets_init', 'applayers_widgets_init' );
 
-function applayers_customize_register( $wp_customize ) {
-    //All our sections, settings, and controls will be added here   
-    
-    $wp_customize->add_setting( 'header_social' , array(
-        'default'   => __( 'Share Your Favorite Mobile Apps With Your Friends', 'applayers' ),
-        'transport' => 'refresh',
-    ) );
+function applayers_widget_categories($args) {
 
-    $wp_customize->add_setting( 'facebook_social' , array(
-        'default'   => __( 'Url facebook', 'applayers' ),
-        'transport' => 'refresh',
-    ) );
+    $walker = new Walker_Categories_Applayers();
+    $args = array_merge($args, array('walker' => $walker));
 
-    $wp_customize->add_setting( 'twitter_social' , array(
-        'default'   => __( 'Url twitter', 'applayers' ),
-        'transport' => 'refresh',
-    ) );
+    return $args;
+}
 
-    $wp_customize->add_setting( 'linkedin_social' , array(
-        'default'   => __( 'Url linkedin', 'applayers' ),
-        'transport' => 'refresh',
-    ) );
-    
-    $wp_customize->add_setting( 'google_social' , array(
-        'default'   => __( 'Url google', 'applayers' ),
-        'transport' => 'refresh',
-    ) );
+add_filter( 'widget_categories_args', 'applayers_widget_categories' );
 
-    $wp_customize->add_setting( 'youtube_social' , array(
-    'default'   => __( 'Url youtube', 'applayers' ),
-    'transport' => 'refresh',
-    ) );
+/**
+ * Classes.
+ */
+require_once get_parent_theme_file_path( 'inc/classes/class-walker-categories-applayers.php' );
 
-    $wp_customize->add_setting( 'instagram_social' , array(
-        'default'   => __( 'Url instagram', 'applayers' ),
-        'transport' => 'refresh',
-    ) );
-
-    $wp_customize->add_setting( 'footer_copy' , array(
-        'default'   => __( '2016 © Copyright Applayers. All rights Reserved. Powered By Free AppLayers Template.', 'applayers' ),
-        'transport' => 'refresh',
-    ) );
-
-    $wp_customize->add_section( 'social_section' , array(
-        'title'      => __( 'Social Settings', 'applayers' ),
-        'priority'   => 30,
-    ) );
-
-    $wp_customize->add_section( 'footer_settings' , array(
-        'title'      => __( 'Footer Settings', 'applayers' ),
-        'priority'   => 31,
-    ) );
-
-    $wp_customize->add_control(
-        'header_social', 
-        array(
-            'label'    => __( 'Social header in footer', 'applayers' ),
-            'section'  => 'social_section',
-            'settings' => 'header_social',
-            'type'     => 'text',
-    ) );
-
-    $wp_customize->add_control(
-        'facebook_social', 
-        array(
-            'label'    => __( 'Facebook profile url', 'applayers' ),
-            'section'  => 'social_section',
-            'settings' => 'facebook_social',
-            'type'     => 'text',
-    ) );
-        
-    $wp_customize->add_control(
-        'twitter_social', 
-        array(
-            'label'    => __( 'Twitter profile url', 'applayers' ),
-            'section'  => 'social_section',
-            'settings' => 'twitter_social',
-            'type'     => 'text',
-    ) );
-
-    $wp_customize->add_control(
-        'linkedin_social', 
-        array(
-            'label'    => __( 'Linkedin profile url', 'applayers' ),
-            'section'  => 'social_section',
-            'settings' => 'linkedin_social',
-            'type'     => 'text',
-    ) );
-
-    $wp_customize->add_control(
-        'google_social', 
-        array(
-            'label'    => __( 'Google profile url', 'applayers' ),
-            'section'  => 'social_section',
-            'settings' => 'google_social',
-            'type'     => 'text',
-    ) );
-        
-    $wp_customize->add_control(
-        'youtube_social', 
-        array(
-            'label'    => __( 'YouTube profile url', 'applayers' ),
-            'section'  => 'social_section',
-            'settings' => 'youtube_social',
-            'type'     => 'text',
-    ) );
-
-    $wp_customize->add_control(
-        'instagram_social', 
-        array(
-            'label'    => __( 'Instagram profile url', 'applayers' ),
-            'section'  => 'social_section',
-            'settings' => 'instagram_social',
-            'type'     => 'text',
-    ) );
-
-    $wp_customize->add_control(
-        'footer_copy', 
-        array(
-            'label'    => __( 'Footer settings', 'applayers' ),
-            'section'  => 'footer_settings',
-            'settings' => 'footer_copy',
-            'type'     => 'textarea',
-    ) );
- }
-
- add_action( 'customize_register', 'applayers_customize_register' );
+/**
+ * Functions.
+ */
+require get_parent_theme_file_path( '/inc/breadcrumbs.php' );
+require get_parent_theme_file_path( '/inc/pagination.php' );
+require get_parent_theme_file_path( '/inc/customizer.php' );
 
  /**
   * JQMIGRATE: Migrate is installed, version 1.4.1
