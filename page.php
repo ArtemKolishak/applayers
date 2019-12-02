9e4cfec80c9aad74b5a1afc0b52dcd8d1c2143f1
Artem Kolishchak <artiom7510@gmail.com>
2019-12-02 18:09:28 +0200

    ADD:page.php, single.php; template-parts: content-page.php, content-single.php, content.php.

--- page.php
+++ page.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * The main template file
+ * The template for displaying all pages
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package AppLayers
+ * @package applayers
  */
-?>
+ ?>
 
-<?php get_header(); ?>
+ <?php get_header(); ?>
 <?php get_template_part( 'template-parts/breadcrumbs' ); ?>
 
 <section class="post_blog_bg primary-bg">
@@ -24,7 +24,12 @@
 					<?php if ( have_posts() ) :?> 
 						<?php while ( have_posts() ) : the_post();
 							
-							get_template_part( 'template-parts/content', get_post_format() );
+							get_template_part( 'template-parts/content', 'page' );
+
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) {
+								comments_template();
+							}
 
 						endwhile; ?>
 					<?php endif; ?>	
@@ -42,6 +47,3 @@
 </section>
 
 <?php get_footer(); ?>
-
-
-
